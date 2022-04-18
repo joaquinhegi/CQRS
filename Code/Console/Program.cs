adcae9214a07babbe66ee3ae7f6ac9fc1baa6af6
Joaquín María Hegi <joaquinhegi@gmai.com>
2022-04-18 18:16:24 -0300

    Add mockup

---
+++ Code/Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.IO;
+
+namespace UIConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
